67e367245a82c18ddce4b7d23ae9d9f3c0d34171
Swetake <41154848+Swetake@users.noreply.github.com>
2018-07-12 22:38:59 +0900

    Add files via upload

---
+++ DesktopVideoRecorder/DesktopVideoRecorder/CommonLibs.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Activities;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+
+namespace DesktopVideoRecorder
+{
+    class CommonLibs
+    {
+        public static void CallPostMessage(IntPtr hWnd, int Msg, int wParam, int lParam)
+        {
+            PostMessage(hWnd, Msg, wParam, lParam);
+        }
+        [DllImport("user32.dll", SetLastError = true)]
+        private static extern bool PostMessage(IntPtr hWnd, int Msg, int wParam, int lParam);
+    }
+}
---
+++ DesktopVideoRecorder/DesktopVideoRecorder/PauseEncoding.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Activities;
+using System.Activities.Statements;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+
+namespace DesktopVideoRecorder
+{
+    public class PauseEncoding : CodeActivity
+    {
+        const string FFMPEG_PROCESS_NAME = "ffmpeg";
+        const int KEY_CHARACTER_CODE_PAUSE = 0x13; // Pause
+        const int WM_KEYDOWN = 0x100;
+
+        [Category("FFmpeg")]
+        [Description(@"Process for FFmpeg.exe. Usually, this is from StartRecording Activity")]
+        [RequiredArgument]
+        public InArgument<Process> FFmpegProcess { get; set; }
+
+        protected override void Execute(CodeActivityContext context)
+        {
+            Pause(FFmpegProcess.Get<Process>(context));
+        }
+
+        public static void Pause(Process ps)
+        {
+
+            if (ps != null && ps.ProcessName.Contains(FFMPEG_PROCESS_NAME))
+            {
+                IntPtr hwnd = ps.MainWindowHandle;
+                CommonLibs.CallPostMessage(hwnd, WM_KEYDOWN, KEY_CHARACTER_CODE_PAUSE, 0);  // send "pause"
+            }
+
+        }
+
+    }
+
+}
