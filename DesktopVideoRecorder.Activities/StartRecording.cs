425db13906fff37b6f473f6207611c6d7f3752ba
Yoichi Suetake <swe@venus.dti.ne.jp>
2018-08-08 19:17:17 +0900

    Correct to retrun proper Process value in FFMpegProcess property.

--- DesktopVideoRecorder.Activities/StartRecording.cs
+++ DesktopVideoRecorder.Activities/StartRecording.cs
@@ -99,6 +99,8 @@ namespace DesktopVideoRecorder
 
                 ps = StartRecording.Start(arguments,DelayAfter.Get(context));
             }
+
+            FFmpegProcess.Set(context, ps);
         }
 
 
