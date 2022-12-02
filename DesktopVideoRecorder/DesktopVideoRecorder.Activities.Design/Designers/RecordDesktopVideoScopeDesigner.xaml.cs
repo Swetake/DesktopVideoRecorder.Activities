eeaf664bbbb58e79cb4fc9a4a4f3d18e50f0a30c
Swetake <41154848+Swetake@users.noreply.github.com>
2022-12-02 23:34:37 +0900

    Add files via upload

---
+++ DesktopVideoRecorder/DesktopVideoRecorder.Activities.Design/Designers/RecordDesktopVideoScopeDesigner.xaml.cs
@@ -0,0 +1,13 @@
+namespace DesktopVideoRecorder.Activities.Design.Designers
+{
+    /// <summary>
+    /// Interaction logic for RecordDesktopVideoScopeDesigner.xaml
+    /// </summary>
+    public partial class RecordDesktopVideoScopeDesigner
+    {
+        public RecordDesktopVideoScopeDesigner()
+        {
+            InitializeComponent();
+        }
+    }
+}
