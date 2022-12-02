38e54424fb7f74994475109ae2fd936bd8a554f6
Swetake <41154848+Swetake@users.noreply.github.com>
2022-12-02 23:28:28 +0900

    Add files via upload

---
+++ DesktopVideoRecorder/DesktopVideoRecorder/FFmpegArgument.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesktopVideoRecorder
+{
+    /// <summary>
+    /// Container of ffmpeg arguments.
+    /// </summary>
+    public class FFmpegArgument
+   {
+        /// <summary>
+        /// Full path to ffmpeg.exe.
+        /// </summary>
+        public string FFmpegFilePath { get; set; }
+
+        /// <summary>
+        /// Full path to output file
+        /// </summary>
+        public string OutputFileName { get; set; }
+
+        /// <summary>
+        /// VideoCodec.
+        /// </summary>
+        public string VideoCodec { get; set; }
+
+        /// <summary>
+        /// PixelFormat
+        /// </summary>
+        public string PixelFormat { get; set; }
+
+        /// <summary>
+        /// ExtraOptionString.
+        /// </summary>
+        public string ExtraOptionString { get; set; }
+
+        /// <summary>
+        /// Max duration.
+        /// </summary>
+        public int MaxDuration { get; set; }
+
+        /// <summary>
+        /// Max filesize.
+        /// </summary>
+        public long MaxFileSize { get; set; }
+
+        /// <summary>
+        /// Frame rate
+        /// </summary>
+        public int FrameRate { get; set; }
+
+        /// <summary>
+        /// Video size.
+        /// </summary>
+        public string VideoSize { get; set; }
+    }
+}
